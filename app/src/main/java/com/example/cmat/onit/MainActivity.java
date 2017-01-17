5e3e1882672f082c41faf82794ff67cbc0dd446a
cmatute <carlos.o.matute@hotmail.com>
2017-01-16 19:48:19 -0500

    front ends update

--- app/src/main/java/com/example/cmat/onit/MainActivity.java
+++ app/src/main/java/com/example/cmat/onit/MainActivity.java
@@ -2,12 +2,48 @@ package com.example.cmat.onit;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
+import android.widget.Toast;
+import android.view.View;
 public class MainActivity extends AppCompatActivity {
+
+    private Spinner spinner1;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
+        spinner1 = (Spinner) findViewById(R.id.spinner1);
+        spinner1.setOnItemSelectedListener(new ItemSelectedListener());
+
+
+    }
+
+    public class ItemSelectedListener implements AdapterView.OnItemSelectedListener {
+        String firstItem = String.valueOf(spinner1.getSelectedItem());
+
+        public void onItemSelected(AdapterView<?> parent, View view, int pos, long id) {
+
+
+            if (firstItem.equals(String.valueOf(spinner1.getSelectedItem()))) {
+
+            } else {
+                Toast.makeText(parent.getContext(),
+                        "YOU HAVE SELECTED: " + parent.getItemAtPosition(pos).toString(),
+                        Toast.LENGTH_LONG).show();
+            }
+
+
+
+        }
+
+        @Override
+        public void onNothingSelected(AdapterView<?> adapterView) {
+
+        }
     }
 }
