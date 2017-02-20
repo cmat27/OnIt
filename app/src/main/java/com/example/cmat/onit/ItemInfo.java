6d696337205fc7820b4cf47b6078220ca93779e9
cmat <cmat27@hotmail.com>
2017-02-20 13:38:38 -0500

    worked on the ItemPosting activity

--- app/src/main/java/com/example/cmat/onit/ItemInfo.java
+++ app/src/main/java/com/example/cmat/onit/ItemInfo.java
@@ -2,17 +2,45 @@ package com.example.cmat.onit;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
 import android.widget.Spinner;
+import android.widget.Toast;
 
-public class ItemInfo extends AppCompatActivity {
+import static android.widget.AdapterView.*;
+
+public class ItemInfo extends AppCompatActivity  {
+
+    private Spinner item_Op;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.item_info);
 
+        item_Op = (Spinner)findViewById(R.id.itemOp);
+        item_Op.setOnItemSelectedListener(new OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+                String test = parent.getItemAtPosition(position).toString();
 
-        final Spinner feedbackSpinner = (Spinner) findViewById(R.id.SpinnerItemType);
-        String feedbackType = feedbackSpinner.getSelectedItem().toString();
+
+                Toast.makeText(parent.getContext(),
+                        "You selected : " + test,
+                        Toast.LENGTH_SHORT).show();
+
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> parent) {
+
+            }
+
+
+        });
+
     }
 }
+
+
